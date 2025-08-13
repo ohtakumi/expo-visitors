d3b0b84c9d03bf7c3b4d962a332e5c96ee279292
ohtakumi <ohtakumi1093@docomo.ne.jp>
2025-08-13 10:33:40 +0900

    修正

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ function showCalendarTable() {
         if ((mm === "04" && dd === "13") || (dd === "01" && month !== prevMonth && mm !== "04")) {
           dateLabel = `
             <div style="font-size:1.1em;font-weight:bold;text-align:left;">
-              <span style="color:#888;font-size:0.9em;font-weight:bold;">${month}月</span>${Number(dd)}日${fireworksIcon}
+              <span style="color:#ffc107;font-size:0.9em;font-weight:bold;">${month}月</span>${Number(dd)}日${fireworksIcon}
             </div>`;
           prevMonth = month;
         } else {
